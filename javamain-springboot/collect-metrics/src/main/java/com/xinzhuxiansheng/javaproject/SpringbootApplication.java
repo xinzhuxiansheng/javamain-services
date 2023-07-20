a2f2893eb448d58475a7b7fe8183b7f94237bf9a
ZhouYang <xinzhuxiansheng@126.com>
2023-07-20 10:14:02 +0800

    add

---
+++ javamain-springboot/collect-metrics/src/main/java/com/xinzhuxiansheng/javaproject/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.xinzhuxiansheng.javaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ConfigurationPropertiesScan({"com.javamain.featuredemos.config"})
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
